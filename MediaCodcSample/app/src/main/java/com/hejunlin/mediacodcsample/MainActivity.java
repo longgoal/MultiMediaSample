b1553986505a69ce5de9175a7ae521ac81c1e0ca
liuxiyu <liuxiyu@hotmail.com>
2020-10-27 16:36:46 +0800

    MediaCodec demo 1，增加一个线程去播放音频

--- MediaCodcSample/app/src/main/java/com/hejunlin/mediacodcsample/MainActivity.java
+++ MediaCodcSample/app/src/main/java/com/hejunlin/mediacodcsample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hejunlin.mediacodcsample;
 
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioFormat;
 import android.media.MediaCodec;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     private static final String SAMPLE = Environment.getExternalStorageDirectory() + "/device-2016-11-15.mp4";
     private static final String TAG = MainActivity.class.getSimpleName();
     private WorkThread mWorkThread = null;
+    private AudioWorkThread mAudioWorkThread = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +49,10 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
             mWorkThread = new WorkThread(holder.getSurface());
             mWorkThread.start();
         }
+        if (mAudioWorkThread == null) {
+            mAudioWorkThread = new AudioWorkThread(holder.getSurface());
+            mAudioWorkThread.start();
+        }
     }
 
     @Override
@@ -55,13 +61,16 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         if (mWorkThread != null) {
             mWorkThread.interrupt();
         }
+        if (mAudioWorkThread != null) {
+            mAudioWorkThread.interrupt();
+        }
     }
 
     private class WorkThread extends Thread {
         private MediaExtractor mMediaExtractor;
         private MediaCodec mMediaCodec;
         private Surface mSurface;
-
+        private String TAG = "VideoWorkThread";
         public WorkThread(Surface surface) {
             this.mSurface = surface;
         }
@@ -69,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         @Override
         public void run() {
             mMediaExtractor = new MediaExtractor();//数据解析器
-            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.video);
+            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.video3);
             try {
                 //mMediaExtractor.setDataSource(SAMPLE);
                 mMediaExtractor.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getDeclaredLength());
@@ -104,8 +113,8 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                 return;
             }
             mMediaCodec.start();//调用start后，如果没有异常信息，就表示成功构建组件
-            ByteBuffer[] inputBuffers = mMediaCodec.getInputBuffers();
-            ByteBuffer[] outputBuffers = mMediaCodec.getOutputBuffers();
+//            ByteBuffer[] inputBuffers = mMediaCodec.getInputBuffers();
+//            ByteBuffer[] outputBuffers = mMediaCodec.getOutputBuffers();
             // 每个buffer的元数据包括具体范围偏移及大小 ，及有效数据中相关解码的buffer
             MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
             boolean isEOS = false;
@@ -118,7 +127,8 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                     //timeoutUs的单位是微秒
                     int inIndex = mMediaCodec.dequeueInputBuffer(10000);//0.01s
                     if (inIndex >= 0) {
-                        ByteBuffer buffer = inputBuffers[inIndex];
+//                        ByteBuffer buffer = inputBuffers[inIndex];
+                        ByteBuffer buffer = mMediaCodec.getInputBuffer(inIndex);
                         Log.d(TAG, ">> buffer " + buffer);
                         int sampleSize = mMediaExtractor.readSampleData(buffer, 0);
                         Log.d(TAG, ">> sampleSize " + sampleSize);
@@ -139,21 +149,24 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                 int outIndex = mMediaCodec.dequeueOutputBuffer(info, 10000);
                 switch (outIndex) {
                     case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED://当buffer变化时，client必须重新指向新的buffer
-                        Log.d(TAG, ">> output buffer changed ");
-                        outputBuffers = mMediaCodec.getOutputBuffers();
+                        Log.d(TAG, ">> output buffer changed 0 ");
+//                        outputBuffers = mMediaCodec.getOutputBuffers();
                         break;
                     case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED://当buffer的封装格式变化,须指向新的buffer格式
-                        Log.d(TAG, ">> output buffer changed ");
+                        Log.d(TAG, ">> output buffer changed 1");
                         break;
                     case MediaCodec.INFO_TRY_AGAIN_LATER://当dequeueOutputBuffer超时,会到达此case
                         Log.d(TAG, ">> dequeueOutputBuffer timeout ");
                         break;
                     default:
-                        ByteBuffer buffer = outputBuffers[outIndex];
+//                        ByteBuffer buffer = outputBuffers[outIndex];
+                        ByteBuffer buffer = mMediaCodec.getOutputBuffer(outIndex);
                         // We use a very simple clock to keep the video FPS, or the video
                         // playback will be too fast
+                        Log.d(TAG, ">> time = "+info.presentationTimeUs / 1000+",systemtime passed="+(System.currentTimeMillis() - startMs));
                         while (info.presentationTimeUs / 1000 > System.currentTimeMillis() - startMs) {
                             try {
+                                Log.d(TAG, ">> time sleep");
                                 sleep(10);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
@@ -174,5 +187,132 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
             mMediaExtractor.release();
         }
     }
+    private class AudioWorkThread extends Thread {
+        private MediaExtractor mMediaExtractor;
+        private MediaCodec mMediaCodec;
+        private Surface mSurface;
+        private AudioPlayer mAudioPlayer;
+        private String TAG = "AudioWorkThread";
+        public AudioWorkThread(Surface surface) {
+            this.mSurface = surface;
+        }
 
+        @Override
+        public void run() {
+            mMediaExtractor = new MediaExtractor();//数据解析器
+            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.video3);
+            try {
+                //mMediaExtractor.setDataSource(SAMPLE);
+                mMediaExtractor.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getDeclaredLength());
+                afd.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            for (int i = 0; i < mMediaExtractor.getTrackCount(); i++) {//遍历数据源音视频轨迹
+                MediaFormat format = mMediaExtractor.getTrackFormat(i);
+                Log.d(TAG, ">> format i " + i + ": " +  format);
+                String mime = format.getString(MediaFormat.KEY_MIME);
+                Log.d(TAG, ">> mime i " + i + ": " +  mime);
+            }
+            for (int i = 0; i < mMediaExtractor.getTrackCount(); i++) {//遍历数据源音视频轨迹
+                MediaFormat format = mMediaExtractor.getTrackFormat(i);
+                Log.d(TAG, ">> format i " + i + ": " +  format);
+                String mime = format.getString(MediaFormat.KEY_MIME);
+                Log.d(TAG, ">> mime i " + i + ": " +  mime);
+                if (mime.startsWith("audio/")) {
+                    mMediaExtractor.selectTrack(i);
+                    try {
+                        mMediaCodec = MediaCodec.createDecoderByType(mime);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    mMediaCodec.configure(format, null, null, 0);
+                    mAudioPlayer = new AudioPlayer(format.getInteger(MediaFormat.KEY_SAMPLE_RATE), AudioFormat.CHANNEL_OUT_STEREO,AudioFormat.ENCODING_PCM_16BIT);
+                    mAudioPlayer.init();
+                    break;
+                }
+            }
+            if (mMediaCodec == null) {
+                return;
+            }
+            mMediaCodec.start();//调用start后，如果没有异常信息，就表示成功构建组件
+//            ByteBuffer[] inputBuffers = mMediaCodec.getInputBuffers();
+//            ByteBuffer[] outputBuffers = mMediaCodec.getOutputBuffers();
+            // 每个buffer的元数据包括具体范围偏移及大小 ，及有效数据中相关解码的buffer
+            MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+            boolean isEOS = false;
+            long startMs = System.currentTimeMillis();
+
+            while (!Thread.interrupted()) {//只要线程不中断
+                if (!isEOS) {
+                    //返回用有效输出的buffer的索引,如果没有相关buffer可用，就返回-1
+                    //如果传入的timeoutUs为0,将立马返回，如果输入buffer可用，将无限期等待
+                    //timeoutUs的单位是微秒
+                    int inIndex = mMediaCodec.dequeueInputBuffer(10000);//0.01s
+                    if (inIndex >= 0) {
+//                        ByteBuffer buffer = inputBuffers[inIndex];
+                        ByteBuffer buffer = mMediaCodec.getInputBuffer(inIndex);
+                        Log.d(TAG, ">> buffer " + buffer);
+                        int sampleSize = mMediaExtractor.readSampleData(buffer, 0);
+                        Log.d(TAG, ">> sampleSize " + sampleSize);
+                        if (sampleSize < 0) {
+                            // We shouldn't stop the playback at this point, just pass the EOS
+                            // flag to mMediaCodec, we will get it again from the
+                            // dequeueOutputBuffer
+                            Log.d(TAG, "InputBuffer BUFFER_FLAG_END_OF_STREAM");
+                            mMediaCodec.queueInputBuffer(inIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+                            isEOS = true;
+                        } else {
+                            mMediaCodec.queueInputBuffer(inIndex, 0, sampleSize, mMediaExtractor.getSampleTime(), 0);
+                            mMediaExtractor.advance();
+                        }
+                    }
+                }
+
+                int outIndex = mMediaCodec.dequeueOutputBuffer(info, 10000);
+                switch (outIndex) {
+                    case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED://当buffer变化时，client必须重新指向新的buffer
+                        Log.d(TAG, ">> output buffer changed 0 ");
+//                        outputBuffers = mMediaCodec.getOutputBuffers();
+                        break;
+                    case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED://当buffer的封装格式变化,须指向新的buffer格式
+                        Log.d(TAG, ">> output buffer changed 1");
+                        break;
+                    case MediaCodec.INFO_TRY_AGAIN_LATER://当dequeueOutputBuffer超时,会到达此case
+                        Log.d(TAG, ">> dequeueOutputBuffer timeout ");
+                        break;
+                    default:
+//                        ByteBuffer buffer = outputBuffers[outIndex];
+                        ByteBuffer buffer = mMediaCodec.getOutputBuffer(outIndex);
+                        // We use a very simple clock to keep the video FPS, or the video
+                        // playback will be too fast
+                        Log.d(TAG, ">> time = "+info.presentationTimeUs / 1000+",systemtime passed="+(System.currentTimeMillis() - startMs));
+                        while (info.presentationTimeUs / 1000 > System.currentTimeMillis() - startMs) {
+                            try {
+                                Log.d(TAG, ">> time sleep");
+                                sleep(10);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                break;
+                            }
+                        }
+                        byte[] outData = new byte[info.size];
+                        buffer.get(outData);
+                        buffer.clear();
+                        mAudioPlayer.play(outData,0,info.size);
+                        mMediaCodec.releaseOutputBuffer(outIndex, true);
+                        break;
+                }
+                // All decoded frames have been rendered, we can stop playing now
+                if ((info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+                    Log.d(TAG, "OutputBuffer BUFFER_FLAG_END_OF_STREAM");
+                    break;
+                }
+            }
+            mMediaCodec.stop();
+            mMediaCodec.release();//释放组件
+            mMediaExtractor.release();
+        }
+    }
 }
